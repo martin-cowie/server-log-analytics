dd20a28db362f3b965773594a75174f857db754c
Martin Cowie <mcowie@pushtechnology.com>
2017-08-11 16:07:50 +0100

    Open a file dialog when no arguments are given. `mvn exec:java` works as expected.

--- src/main/java/com/pushtechnology/support/logfiledigest/gui/Main.java
+++ src/main/java/com/pushtechnology/support/logfiledigest/gui/Main.java
@@ -3,16 +3,23 @@ package com.pushtechnology.support.logfiledigest.gui;
 import static com.pushtechnology.support.logfiledigest.LogfileDigester.DATE_FORMAT;
 import static com.pushtechnology.support.logfiledigest.gui.TimeSeriesGraphsPalette.showGraphPalette;
 import static java.lang.Integer.parseInt;
+import static java.util.Arrays.asList;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.prefs.Preferences;
+import java.util.stream.Collectors;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -25,6 +32,11 @@ import com.pushtechnology.support.logfiledigest.LogfileDigester;
 
 public class Main {
     private static final String DATE = "Date";
+    private static final String LOGS_DIR = "last.logs.dir";
+    private static final Preferences PREFS = Preferences.userNodeForPackage(Main.class);
+
+    private static final FileNameExtensionFilter LICENCE_LOG_FILES =
+        new FileNameExtensionFilter("Diffusion logs", "log", "csv");
 
     private static List<XYDataset> toDataSets(List<CSVRecord> records) throws ParseException {
         // Pre-populate the set of series "serieses", misspelled for clarity.
@@ -75,28 +87,46 @@ public class Main {
 
     //TODO: swap File for Path
     public static void main(String args[]) throws Exception {
-        if (args.length < 1) {
-            System.err.println("wrong # args");
-            System.exit(1);
+        if (args.length > 0) {
+            process((asList(args)
+                .stream()
+                .map(File::new)
+                .collect(Collectors.toList())));
+        }
+        else {
+            // Open an Open File dialog instead
+            final String dir = PREFS.get(LOGS_DIR, null);
+            final JFileChooser chooser = new JFileChooser(dir);
+            chooser.setFileFilter(LICENCE_LOG_FILES);
+            chooser.setMultiSelectionEnabled(true);
+            chooser.setDialogTitle("Select log or CSV files");
+            chooser.setApproveButtonText("Consume");
+
+            if (JFileChooser.APPROVE_OPTION == chooser.showOpenDialog(null)) {
+                // workingDirectory = chooser.getCurrentDirectory();
+                process(asList(chooser.getSelectedFiles()));
+                PREFS.put(LOGS_DIR, chooser.getCurrentDirectory().toString());
+            }
         }
 
+    }
+
+    private static void process(List<File> files) throws FileNotFoundException, IOException, ParseException {
         final ArrayList<CSVRecord> csvRecords = new ArrayList<CSVRecord>();
+        for(File arg : files) {
 
-        for(String arg : args) {
-
-            if(arg.toLowerCase().endsWith(".csv")) {
-                System.err.println("Consuming " + arg);
-                final FileReader reader = new FileReader(new File(arg));
+            if(arg.getName().toLowerCase().endsWith(".csv")) {
+                final FileReader reader = new FileReader(arg);
                 CSVFormat.RFC4180
                     .withFirstRecordAsHeader()
                     .withIgnoreSurroundingSpaces(true)
                     .parse(reader)
                     .forEach(csvRecords::add);
 
-            } else if(arg.toLowerCase().endsWith(".log")) {
+            } else if(arg.getName().toLowerCase().endsWith(".log")) {
                 //Convert to CSV on the fly
                 System.err.println("Handling " + arg);
-                final File csvFilename = LogfileDigester.digest(new File(arg));
+                final File csvFilename = LogfileDigester.digest(arg);
                 System.err.println("Consuming temporary file " + csvFilename);
                 final FileReader reader = new FileReader(csvFilename);
                 CSVFormat.RFC4180
@@ -110,11 +140,11 @@ public class Main {
         }
 
         if (csvRecords.isEmpty()) {
-            System.err.println("Gathered no records from " + Arrays.toString(args));
+            System.err.println("Gathered no records from " + files);
             System.exit(1);
         }
 
-        final String title = args.length == 1 ? new File(args[0]).getName() : args.length + " files";
+        final String title = files.size() == 1 ? files.get(0).getName() : files.size() + " files";
         showGraphPalette(title,
             toDataSets(csvRecords),
             timeRange(csvRecords));
